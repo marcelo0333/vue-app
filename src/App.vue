ce50774036da8f0d812ede35102c0008d4c82a1c
marcelo0333 <139006301+marcelo0333@users.noreply.github.com>
2025-05-22 15:01:19 -0300

    feat: events

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,69 @@
+<template>
+  <div>
+    <button
+      @click="onClick"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Enviar
+    </button>
+  </div>
+  <div
+    @mouseover="onMouseover"
+    @mouseout="onMouseout"
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Passa aqui
+  </div>
+  <div>
+    <form action="https://google.com" @submit.once="onClick">
+      <input @keyup="onKeyUp"
+        type="text"
+        name="name"
+        id="name"
+        class="bg-gray-200 border border-gray-300 rounded py-2 px-4 mb-4"
+        placeholder="Nome">
+      <button
+        type="submit"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Enviar
+      </button>
+    </form>
+  </div>
+</template>
+
 <!-- eslint-disable vue/block-lang -->
 <script>
-import HeaderApp from './components/HeaderApp.vue'
+// import HeaderApp from './components/HeaderApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderApp,
+    // HeaderApp,
   },
   data() {
     return {
-
       showHeader: true,
-      imgAlt: 'Logos',
-      todos: [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            imgSrc: 'https://cdn.discordapp.com/attachments/1131950982032050226/1159996586489828352/diabgeet.png',
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-          }
-    ]
     }
-
+  },
+  methods: {
+    onClick($event) {
+      console.log('click', $event)
+      // $event.preventDefault();
+      // alert('clicou');
+      // this.showHeader = !this.showHeader;
+    },
+    onMouseover() {
+      console.log('mouseover')
+    },
+    onMouseout() {
+      console.log('mouseout')
+    },
+     onKeyUp($event) {
+      console.log('keyup', $event)
+    },
   },
 }
 </script>
 
-<template>
-  <HeaderApp v-if="showHeader" />
-
-  <img
-  :src="imgSrc"
-  :alt="imgAlt"
-  />
-    <div v-for="local in todos"
-    v-bind:key="local.id"
-    class="bg-gray-100 p-4 rounded-lg shadow-md mb-4"
-    >
-      <img
-        v-if="local.imgSrc"
-        :src="local.imgSrc"
-        :alt="imgAlt"
-        class="w-16 h-16 mb-2 rounded-full"
-      />
-      <h2 class="text-xl font-semibold mb-2">{{ local.title }}</h2>
-      <p class="text-gray-600 mb-2">User ID: {{ local.userId }}</p>
-      <p class="text-gray-600 mb-2">ID: {{ local.id }}</p>
-      <p class="text-gray-600 mb-2">Completed: </p>
-      <p v-if="local.completed" class="text-green-500">Yes</p>
-      <p v-else class="text-red-500">No</p>
-      <p>{{ local.completed }}</p>
-    </div>
-</template>
-
-<style scoped>
-</style>
+<style scoped></style>
