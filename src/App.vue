09f280e9dd7471f8d5652be2953ce91872310751
marcelo0333 <139006301+marcelo0333@users.noreply.github.com>
2025-05-22 01:01:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<!-- eslint-disable vue/block-lang -->
+<script>
+import HeaderApp from './components/HeaderApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderApp,
+  },
+  data() {
+    return {
+
+      showHeader: true,
+      imgAlt: 'Logos',
+      todos: [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            imgSrc: 'https://cdn.discordapp.com/attachments/1131950982032050226/1159996586489828352/diabgeet.png',
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+          }
+    ]
+    }
+
+  },
+}
+</script>
+
+<template>
+  <HeaderApp v-if="showHeader" />
+
+  <img
+  :src="imgSrc"
+  :alt="imgAlt"
+  />
+    <div v-for="local in todos"
+    v-bind:key="local.id"
+    class="bg-gray-100 p-4 rounded-lg shadow-md mb-4"
+    >
+      <img
+        v-if="local.imgSrc"
+        :src="local.imgSrc"
+        :alt="imgAlt"
+        class="w-16 h-16 mb-2 rounded-full"
+      />
+      <h2 class="text-xl font-semibold mb-2">{{ local.title }}</h2>
+      <p class="text-gray-600 mb-2">User ID: {{ local.userId }}</p>
+      <p class="text-gray-600 mb-2">ID: {{ local.id }}</p>
+      <p class="text-gray-600 mb-2">Completed: </p>
+      <p v-if="local.completed" class="text-green-500">Yes</p>
+      <p v-else class="text-red-500">No</p>
+      <p>{{ local.completed }}</p>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="bg-gray-800 text-white p-4 flex justify-between items-center">
+    <h1 class="text-2xl font-bold">Diabgeet</h1>
+    <nav>
+      <ul class="flex space">
+        <li class="mr-4"><a href="#" class="text-white hover:text-gray-400">Home</a></li>
+        <li class="mr-4"><a href="#" class="text-white hover:text-gray-400">About</a></li>
+        <li class="mr-4"><a href="#" class="text-white hover:text-gray-400">Contact</a></li>
+        <li><a href="#" class="text-white hover:text-gray-400">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
