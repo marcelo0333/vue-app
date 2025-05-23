7f82836b1c43ee3f75cfa3819dd8b7c447d44d80
marcelomw3 <marcelomwenceslau@gmail.com>
2025-05-23 10:10:47 -0300

    events

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,54 @@
+
+
+<template>
+  <HeaderApp v-if="showHeader" />
+  <div>
+    One Wat data binding <br>
+    Two-way data binding<br>
+    v-model -> formulários
+  </div>
+
+  <div class="text-gray hover:text-gray-400">
+    <label >  Nome  </label>
+      <input
+      class="browser-default border-2 border-gray-300 rounded-lg p-2"
+      type="text"
+      v-model="name">
+      <br>
+    <p>{{ name }}</p>
+
+  </div>
+  <div class>
+      <label >  Nome  </label>
+        <input
+        class="browser-default border-2 border-gray-300 rounded-lg p-2"
+        type="text"
+        v-model="name">
+        <br>
+      <p>{{ name }}</p>
+  </div>
+
+  <div>
+    <label for="sport">Choose a sport:</label>
+    <select v-model="sports" id="sport">
+      <option value="">Escolha</option>
+      <option value="soccer">Soccer</option>
+      <option value="basketball">Basketball</option>
+      <option value="tennis">Tennis</option>
+      <option value="baseball">Baseball</option>
+    </select>
+    {{ sports.text }}
+  </div>
+
+  <div>
+
+    <label> Cores</label>
+    <input type="checkbox" value="azul" v-model="colors"> azul
+     <input type="checkbox" value="amarelo" v-model="colors"> amarelo
+    {{ colors }}
+  </div>
+
+</template>
 <!-- eslint-disable vue/block-lang -->
 <script>
 import HeaderApp from './components/HeaderApp.vue'
@@ -9,74 +60,20 @@ export default {
   },
   data() {
     return {
-
       showHeader: true,
-      imgAlt: 'Logos',
-      todos: [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            imgSrc: 'https://cdn.discordapp.com/attachments/1131950982032050226/1159996586489828352/diabgeet.png',
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-          }
-    ]
+      name: 'Marcelo',
+      var: 'text',
+      sports: '',
+      colors: [],
     }
-
-  },
+  }
 }
 </script>
-
-<template>
-  <HeaderApp v-if="showHeader" />
-
-  <img
-  :src="imgSrc"
-  :alt="imgAlt"
-  />
-    <div v-for="local in todos"
-    v-bind:key="local.id"
-    class="bg-gray-100 p-4 rounded-lg shadow-md mb-4"
-    >
-      <img
-        v-if="local.imgSrc"
-        :src="local.imgSrc"
-        :alt="imgAlt"
-        class="w-16 h-16 mb-2 rounded-full"
-      />
-      <h2 class="text-xl font-semibold mb-2">{{ local.title }}</h2>
-      <p class="text-gray-600 mb-2">User ID: {{ local.userId }}</p>
-      <p class="text-gray-600 mb-2">ID: {{ local.id }}</p>
-      <p class="text-gray-600 mb-2">Completed: </p>
-      <p v-if="local.completed" class="text-green-500">Yes</p>
-      <p v-else class="text-red-500">No</p>
-      <p>{{ local.completed }}</p>
-    </div>
-</template>
-
 <style scoped>
+
+.text{
+  color: aliceblue;
+}
+
+
 </style>
